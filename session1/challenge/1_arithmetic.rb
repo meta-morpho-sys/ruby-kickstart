13575a3b3498d52a1d432d34c99610eb921eb037
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-12 10:57:48 +0100

    Exercise 1

--- session1/challenge/1_arithmetic.rb
+++ session1/challenge/1_arithmetic.rb
@@ -9,5 +9,10 @@
 # arithmeitc1(-6)     # => -50
 
 def arithmetic1(n)
-  # the code for this method goes in here
+  n * 5 -20
 end
+
+p arithmetic1 10
+p arithmetic1 10.5
+p arithmetic1 -6
+p arithmetic1 100
